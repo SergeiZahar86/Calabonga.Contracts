3e0ea733afe57ee619e0f1d26ab2c2be136ec24d
Сергей Захаров <sergei86zahar@mail.ru>
2021-08-15 08:53:27 +0700

    Constants

---
+++ Calabonga.Contracts/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Calabonga.Contracts
+{
+    public static class Constants
+    {
+        public static string NotificationQueueNameConfiguration = "NotificationQueueNameConfiguration";
+        public static string NotificationQueueNameWarehouse = "NotificationQueueNameWarehouse";
+    }
+}
