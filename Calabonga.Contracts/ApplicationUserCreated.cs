abf206d6a37f0b5ba24f1741d4ec3b7d3c5c88be
Сергей Захаров <sergei86zahar@mail.ru>
2021-08-15 08:34:43 +0700

    Добавьте файлы проекта.

---
+++ Calabonga.Contracts/ApplicationUserCreated.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Calabonga.Contracts
+{
+    public class ApplicationUserCreated : IApplicationUserCreated
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Calabonga.Contracts/IApplicationUserCreated.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calabonga.Contracts
+{
+    public interface IApplicationUserCreated
+    {
+        /// <summary>
+        /// Identifier
+        /// </summary>
+        Guid Id { get; set; }
+        /// <summary>
+        /// FirstName
+        /// </summary>
+        string FirstName { get; set; }
+
+        /// <summary>
+        /// LastName
+        /// </summary>
+        string LastName { get; set; }
+    }
+}
